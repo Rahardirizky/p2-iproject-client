2f66149030059f1850c61c50b0740e9139e32e33
Rahardirizky <rahardirizky@gmail.com>
2021-11-17 23:02:02 +0700

    create login features

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,53 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiredAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, _, next) => {
+  const token = localStorage.token;
+  if (to.matched.some((record) => record.meta.requiredAuth)) {
+    if (token) {
+      next();
+    } else {
+      next({
+        path: "/login",
+      });
+    }
+  } else if (to.path === "/login" || to.path === "/register") {
+    if (token) {
+      next({
+        path: "/",
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
